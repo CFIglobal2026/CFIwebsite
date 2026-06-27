fe98107e92fa07067c97b479fb6b74e69dd033cf
cfiglobal <ceo@cfiglobal.in>
2026-06-27 21:40:51 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,59 @@
+const employees = [
 
+{
+id: "CFI001",
+password: "12345",
+
+name: "Anisha ",
+
+department: "Information Technology",
+
+contact: "+91 9350140063",
+
+email: "anisha@cfiglobal.com",
+
+manager: "Aakash Yadav ",
+
+phase1: 85,
+
+phase2: 90,
+
+phase3: 95,
+
+percentage: "90%",
+
+grade: "A+",
+
+status: "PASS"
+
+},
+
+{
+id: "CFI002",
+password: "54321",
+
+name: "Nikita Singh",
+
+department: "Human Resource",
+
+contact: "+91 9876501234",
+
+email: "nikita@cfiglobal.com",
+
+manager: "Aakash Yadav",
+
+phase1: 80,
+
+phase2: 82,
+
+phase3: 88,
+
+percentage: "83%",
+
+grade: "A",
+
+status: "PASS"
+
+}
+
+];
